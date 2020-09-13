99cdbaf6f3bf8c3fd51c6d130ffb56214ac26d06
Dalton Natale <cdalton.natale@gmail.com>
2020-09-13 10:06:41 -0700

    burgers

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,7 @@
+const express = require("express");
+
+const burger = require("../models/burgers.js");
+
+const router = express.Router();
+
+router.get("/", function () {});
---
+++ db/seeds.sql
@@ -0,0 +1,13 @@
+USE burgers_db
+
+INSERT INTO burgers (
+    burger_name,
+    devoured 
+) VALUES ("Diablo Burger", false);
+INSERT INTO burgers (
+    burger_name,
+    devoured 
+) VALUES ("Big MAC", false);INSERT INTO burgers (
+    burger_name,
+    devoured 
+) VALUES ("Double Double", false);
